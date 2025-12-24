ce53e07d2ad12796a8400c0146866ef604d512a0
MD SAQUIB ANSARI <mdshaanansari0786@gmail.com>
2025-12-24 18:27:45 +0530

    fix: update server port from 3000 to 3001

--- Chapter 7 - npm and Tools/app.js
+++ Chapter 7 - npm and Tools/app.js
@@ -3,7 +3,7 @@ const server = http.createServer((req, res) => {
   console.log(req);
 });
 
-const PORT = 3000;
+const PORT = 3001;
 server.listen(PORT, () => {
   console.log(`Server is listening on address hhtp://localhost:${PORT}`);
 });
