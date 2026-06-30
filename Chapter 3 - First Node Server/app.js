5a4e413d27b83624afab8a36a1d4ee6b72a0addd
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-06-30 11:57:58 +0530

    feat: enhance request handling by adding response for '/shopping' route

--- Chapter 3 - First Node Server/app.js
+++ Chapter 3 - First Node Server/app.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  process.exit();
+  console.log(req.url, req.method,req.headers);
+  
 });
 
 const port = 3001;
--- Chapter 4 - Request and Response/app.js
+++ Chapter 4 - Request and Response/app.js
@@ -18,14 +18,16 @@ const server = http.createServer((req, res) => {
      res.write('</html>');
      return res.end();
 
+  } else if (req.url === '/shopping') {
+     res.setHeader('Content-Type', 'text/html');
+     res.write('<html>');
+     res.write('<head><tittle>Complete Coding</tittle></head>');
+     res.write('<body><h1>Thanks for the shopping.</h1></body>');
+     res.write('</html>');
+     return res.end();
   } 
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><tittle>Complete Coding</tittle></head>');
-  res.write('<body><h1>Like / Share / Subscribe</h1></body>');
-  res.write('</html>');
-  res.end();
 });
+
 const port = 3001;
 server.listen(port, () => {
   console.log(`Server running on address http://localhost:${port}`);
