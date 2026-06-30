c89630c06a9798980718e0c569691ec695d9a793
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-06-30 11:28:56 +0530

    fix: clean up code formatting and add newline at end of first.js

--- Chapter 2 First NodeJS Program/first.js
+++ Chapter 2 First NodeJS Program/first.js
@@ -1,7 +1,5 @@
-
-
 console.log("KG Coding is the best...");
-const fs = require('fs');
+const fs = require("fs");
 fs.writeFile("output.txt", "Writing File", (err) => {
   if (err) console.log("ERROR OCCURED");
   else console.log("File Written Successfully");
--- Chapter 3 - First Node Server/app.js
+++ Chapter 3 - First Node Server/app.js
@@ -2,7 +2,9 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req);
+  process.exit();
 });
+
 const port = 3001;
 server.listen(port, () => {
   console.log(`Server running on address http://localhost:${port}`);
