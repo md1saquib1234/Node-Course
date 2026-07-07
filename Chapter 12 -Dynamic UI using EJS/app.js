ab198cad88edf9336ce7f1c4413e280abc490d81
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-07-07 20:41:22 +0530

    feat: implement 404 error page using EJS and refactor head and header partials

--- Chapter 12 -Dynamic UI using EJS/app.js
+++ Chapter 12 -Dynamic UI using EJS/app.js
@@ -27,7 +27,7 @@ app.use("/host",hostRouter);
 
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+  res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 
--- Chapter 12 -Dynamic UI using EJS/routes/userRouter.js
+++ Chapter 12 -Dynamic UI using EJS/routes/userRouter.js
@@ -11,7 +11,7 @@ const { registeredHomes } = require('./hostRouter');
 
 userRouter.get("/", (req, res, next) => {
   console.log(registeredHomes);
-  res.render('home', {registeredHomes: registeredHomes});
+  res.render('home', {registeredHomes: registeredHomes, pageTitle: 'airbnb Home'});
 });
 
 module.exports = userRouter;
