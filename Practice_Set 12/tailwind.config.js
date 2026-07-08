88c346e979bc95b8e937c9e27acfa23a3e1db3e7
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-07-08 16:20:08 +0530

    feat: update addHome.ejs form to include house name and price fields

--- Practice_Set 12/tailwind.config.js
+++ Practice_Set 12/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/*.html", "./views/*.ejs"],
+  content: ["./views/*.html", "./views/**/*.ejs"],
   theme: {
     extend: {},
   },
