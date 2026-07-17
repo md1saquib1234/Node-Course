b401bac8554a0ff39fa4d734f1b7c60900e9a714
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-07-17 18:08:26 +0530

    Add routing and views for host and store functionalities
    - Created hostRouter and storeRouter for handling routes related to homes and bookings.
    - Implemented views for adding homes, listing homes, and displaying 404 error.
    - Added partials for head and header to maintain consistent layout across pages.
    - Integrated Tailwind CSS for styling and responsive design.
    - Developed forms for home registration and booking functionalities.
    - Established utility functions for path management.

---
+++ Chapter 14 - Dynamic Paths/app.js
@@ -0,0 +1,37 @@
+//Core modules
+const path = require('path');
+
+// External modules
+const express = require('express');
+
+
+// Local Modules
+const storeRouter = require('./routes/storeRouter');
+const hostRouter = require('./routes/hostRouter');
+const rootDir = require("./utils/pathUtil");
+const errorsController = require("./controllers/errors");
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(express.static(path.join(rootDir, 'public' )));
+
+app.use(express.urlencoded());
+
+app.use(storeRouter);
+app.use("/host",hostRouter);
+
+
+
+
+app.use(errorsController.pageNotFound);
+
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on address http://localhost:${PORT}`);
+});
