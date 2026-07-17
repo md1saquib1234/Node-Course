5116d2cbbbade83ac756d328d7da3d0befe52024
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-07-17 11:57:35 +0530

    feat: enhance home management by updating JSON data structure and improving save functionality

--- Chapter 13 - MVC/controllers/homes.js
+++ Chapter 13 - MVC/controllers/homes.js
@@ -1,21 +1,35 @@
 const Home = require("../models/home");
 
 exports.getAddHome = (req, res, next) => {
-  res.render('addHome', {pageTitle: 'Add Home to airbnd', currentPage: 'addHome'});
-}
-
+  res.render("addHome", {
+    pageTitle: "Add Home to airbnd",
+    currentPage: "addHome",
+  });
+};
 
 exports.postAddHome = (req, res, next) => {
-  console.log('Home Registration Successful for:', req.body);
-  const home = new Home(req.body.houseName, req.body.price, req.body.location, req.body.rating, req.body.photoUrl);
+  console.log("Home Registration Successful for:", req.body);
+  const home = new Home(
+    req.body.houseName,
+    req.body.price,
+    req.body.location,
+    req.body.rating,
+    req.body.photoUrl,
+  );
   home.save();
 
-  res.render('homeAdded', {pageTitle: 'Home Added Successfully', currentPage: 'homeAdded'});
+  res.render("homeAdded", {
+    pageTitle: "Home Added Successfully",
+    currentPage: "homeAdded",
+  });
 };
 
 exports.getHomes = (req, res, next) => {
-  const registeredHomes = Home.fetchAll();
-  console.log(registeredHomes);
-  res.render('home', {registeredHomes: registeredHomes, pageTitle: 'airbnb Home', currentPage: 'Home'});
-}
-
+  Home.fetchAll((registeredHomes) =>
+    res.render("home", {
+      registeredHomes: registeredHomes,
+      pageTitle: "airbnb Home",
+      currentPage: "Home",
+    }),
+  );
+};
--- Chapter 13 - MVC/models/home.js
+++ Chapter 13 - MVC/models/home.js
@@ -3,8 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const rootDir = require('../utils/pathUtil');
 
-// fake database
-const registeredHomes = [];
+
 
 module.exports = class Home {
     constructor(houseName, price, location, rating, photoUrl) {
@@ -17,16 +16,23 @@ module.exports = class Home {
     }
  
     save() {
-        registeredHomes.push(this);
+        Home.fetchAll(registeredHomes => {
+         registeredHomes.push(this);
         const homeDataPath = path.join(rootDir, 'data', 'homes.json');
         fs.writeFile(homeDataPath, JSON.stringify(registeredHomes), error => {
-            console.log("File Writing Concluded", error);
+            
         });
+        })
+       
     }
 
      
-    static fetchAll() {
-        return registeredHomes;
+    static fetchAll(callback) {
+        const homeDataPath = path.join(rootDir, 'data', 'homes.json');
+        fs.readFile(homeDataPath, (err, data) => {
+            callback(!err ? JSON.parse(data) : []);
+        });
+         
     }
 
 }
