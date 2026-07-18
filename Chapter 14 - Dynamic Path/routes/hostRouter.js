d0dc2730362ca668540629cffb908b4b6921c503
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-07-18 11:48:47 +0530

    Add host and store routers, views, and utility functions for dynamic path handling
    - Created hostRouter and storeRouter for handling routes related to hosts and homes.
    - Added views for 404 error, adding homes, home list, and home registration confirmation.
    - Implemented partials for head and header to maintain consistent layout across views.
    - Introduced utility function for path management.
    - Configured Tailwind CSS for styling.
    - Developed views for displaying homes, bookings, and favourites with dynamic data rendering.

---
+++ Chapter 14 - Dynamic Path/routes/hostRouter.js
@@ -0,0 +1,13 @@
+
+//External Modules
+const express = require('express');
+const hostRouter = express.Router();
+
+//Locale Modules
+const hostController = require("../controllers/hostController");
+
+hostRouter.get("/add-home", hostController.getAddHome );
+hostRouter.post("/add-home", hostController.postAddHome);
+hostRouter.get("/host-home-list", hostController.getHostHomes);
+
+module.exports = hostRouter;
